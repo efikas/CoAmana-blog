032938f322f2300c282dccf43174b95ac5bf1c09
Efikas <latyf01@gmail.com>
2019-08-30 16:30:14 +0100

    redux code fixed

--- src/components/pages/BlogPost/BlogPost.js
+++ src/components/pages/BlogPost/BlogPost.js
@@ -15,14 +15,18 @@ class BlogPost extends Component {
 
   normalizePost = () => {
 
+    //if posts is empty return to homepage
     if(this.props.posts.length < 1) {
       this.props.history.push("/");
+      return;
     }
 
     let post = this.props.posts.filter(pos => pos.slug == this.props.match.params.slug);
 
+    //if post is not in posts return to homepage
     if (post.length < 1) {
       this.props.history.push("/");
+      return;
     }
     return post[0];
   }
@@ -31,19 +35,25 @@ class BlogPost extends Component {
     const post = this.normalizePost();
 
     return (
-      <div className="app flex-row align-items-center">
-         <header className={classes.sheader}>
-          <h1 className="text-center pt-5">{post.title.rendered}</h1>
-        </header>
-        <Container>
-          <div className="mb-4">
-              <CardImg top width="100%" src={post.featured_image} alt={post.title.rendered} />
-               
-              <CardBody>
-                  <CardText dangerouslySetInnerHTML={{__html: post.content.rendered}}></CardText>
-              </CardBody>
-          </div>
-        </Container>
+      <div>
+          {
+            post && 
+            
+            <div className="app flex-row align-items-center">
+                  <header className={classes.sheader}>
+                  <h1 className="text-center pt-5">{post.title.rendered}</h1>
+                </header>
+                <Container>
+                  <div className="mb-4">
+                      <CardImg top width="100%" src={post.featured_image} alt={post.title.rendered} />
+                        
+                      <CardBody>
+                          <CardText dangerouslySetInnerHTML={{__html: post.content.rendered}}></CardText>
+                      </CardBody>
+                  </div>
+                </Container>
+              </div>
+          }
       </div>
     );
   }
@@ -54,7 +64,5 @@ const mapStateToProps = (state) => {
       posts: state.posts,
   }
 }
-
-
 
 export default connect(mapStateToProps, null)(BlogPost);
